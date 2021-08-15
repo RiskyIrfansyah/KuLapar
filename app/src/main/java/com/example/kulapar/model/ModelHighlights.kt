dc38f1d0a31f256290a89426db982f3bccf56e58
Risky Irfansyah <kikiirfansyah39@gmail.com>
2021-08-15 23:53:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kulapar/model/ModelHighlights.kt
@@ -0,0 +1,7 @@
+package com.example.kulapar.model
+
+import java.io.Serializable
+
+class ModelHighlights : Serializable {
+    var highlights: String? = null
+}
---
+++ app/src/main/java/com/example/kulapar/utils/OnItemClickCallback.kt
@@ -0,0 +1,7 @@
+package com.example.kulapar.utils
+
+import com.example.kulapar.model.ModelMain
+
+interface OnItemClickCallback {
+    fun onItemMainClicked(modelMainHorizontal: ModelMain?)
+}
